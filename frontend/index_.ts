a07e2e860e3d2b11ca17861dc4e0e1755ff8710f
Luciano Vernaschi <luciano@vaadin.com>
2023-02-06 22:08:17 +0100

    More housekeeping

--- frontend/index_.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from '@vaadin/router';
-
-export const router = new Router(document.querySelector('#outlet'));
-
-window.addEventListener('vaadin-router-location-changed', (e) => {
-  console.log('Location changed', e.detail.location);
-});
